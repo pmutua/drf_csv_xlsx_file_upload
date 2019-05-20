2beb9d6357d48cf50d66c633617a29b29f421392
Philip Mutua <pmutua@live.com>
2019-05-20 10:52:45 +0300

    Add view remaining with logic to read csv.

--- drf_upload_csv/patients/views.py
+++ drf_upload_csv/patients/views.py
@@ -1,3 +1,19 @@
+from .serializers import *
 from django.shortcuts import render
+from rest_framework.parsers import FileUploadParser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class FileuploadView(APIView):
+    parser_classes = (FileUploadParser,)
+
+    def put(self, request, filename, format=None):
+        """Upload the CSV file.
+
+        Then reads it and saves data to database.
+        """
+        file_obj = request.data['file']
+
+        return Response(status=status.HTTP_200_OK)
