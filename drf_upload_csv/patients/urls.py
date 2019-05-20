8521f43e2f5e433ea3478ff62367c0f68781ba53
Philip Mutua <pmutua@live.com>
2019-05-20 16:50:56 +0300

    Added urls.

--- drf_upload_csv/patients/urls.py
+++ drf_upload_csv/patients/urls.py
@@ -1,7 +1,8 @@
+"""Contains all urls for patients app."""
 from django.urls import re_path
-from rest_framework.urlpatterns import format_suffix_patterns
 from patients import views
 
 urlpatterns = [
-    re_path(r'^upload/(?P<filename>[^/]+)$', views.FileUploadView.as_view())
+    re_path(r'^csv_upload/$', views.FileUploadView.as_view()),
+    re_path(r'^list/$', views.PatientListAPIView.as_view())
 ]
