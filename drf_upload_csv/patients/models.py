a675416be13b19db519ea943fcd4a245c43a3d2a
Philip Mutua <pmutua@live.com>
2019-05-20 16:45:45 +0300

    Added model definitions.

--- drf_upload_csv/patients/models.py
+++ drf_upload_csv/patients/models.py
@@ -1,4 +1,15 @@
+"""All Model definitions of patients app."""
 from django.db import models
+
+
+class User(models.Model):
+    """Represents User class Model."""
+
+    uuid = models.CharField(max_length=250)
+
+    def __str__(self):
+        """Return user uuid."""
+        return self.uuid
 
 
 class Patient(models.Model):
@@ -13,6 +24,23 @@ class Patient(models.Model):
     :type email: string
     :param email: the email of the patient
     """
+
     firstname = models.CharField(max_length=250)
     lastname = models.CharField(max_length=250)
     email = models.CharField(max_length=250)
+
+    def __str__(self):
+        """Return firstname and last name."""
+        return " {}-{} ".format(self.firstname, self.lastname)
+
+
+class FileUpload(models.Model):
+    """Represents file upload model class."""
+
+    owner = models.ForeignKey(User, to_field='id', on_delete=models.CASCADE)
+    file = models.FileField(upload_to='csv_uploads/%y/%m')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return file name."""
+        return self.file.name
