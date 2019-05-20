705ba25d449924460c4a180085ecb70e43407de1
Philip Mutua <pmutua@live.com>
2019-05-20 10:36:43 +0300

    Added doc strings.

--- drf_upload_csv/patients/models.py
+++ drf_upload_csv/patients/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Patient(models.Model):
     """This Model represents a Patient class.
 
-    :type name: string
-    :param name: the name of the department
+    :type firstname: string
+    :param firstname: the firstname of the patient
+
+    :type lastname: string
+    :param lastname: the lastname of the patient
+
+    :type email: string
+    :param email: the email of the patient
     """
     firstname = models.CharField(max_length=250)
     lastname = models.CharField(max_length=250)
