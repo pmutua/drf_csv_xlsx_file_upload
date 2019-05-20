394a7f01a76353830d0237f75ca3cba1ed5a5107
Philip Mutua <pmutua@live.com>
2019-05-20 16:49:45 +0300

    Added serializers.

--- drf_upload_csv/patients/serializers.py
+++ drf_upload_csv/patients/serializers.py
@@ -1,10 +1,27 @@
-from .models import *
+"""This file contains all serializers for patients app."""
+from .models import (
+    Patient,
+    FileUpload
+)
+
 from rest_framework import serializers
 
 
-class PatientSerializer(serializers.Serializer):
+class PatientSerializer(serializers.ModelSerializer):
     """Represents patient serializer class."""
 
     class Meta:
+        """Contains model & fields used by this serializer."""
+
         model = Patient
+        fields = '__all__'
+
+
+class FileSerializer(serializers.ModelSerializer):
+    """Represents file upload serializer class."""
+
+    class Meta:
+        """Contains model & fields used by this serializer."""
+
+        model = FileUpload
         fields = '__all__'
